245ff635a4a6eb62cfef6dac584a53ab412ab394
Poznanskiy Denis <dpoznanskiy@yandex.ru>
2023-04-11 23:58:45 +0300

    example2

--- src/2. advanced types/example2.ts
+++ src/2. advanced types/example2.ts
@@ -1,9 +1,15 @@
 type Split<S extends string, Separator extends string> =
     S extends `${infer Word}${Separator}${infer Rest}`
         ? [Word, ...Split<Rest, Separator>]
-        : []
+        : S extends ''
+            ? Separator extends ''
+                ? []
+                : [S]
+            : string extends S
+                ? S[]
+                : [S]
 
-let example_test1: Split<'Hi! world!', 'w'>;  // ["Hi! "]
-let example_test2: Split<'Hi! world!', ''>; //  ["H", "i", "!", " ", "w", "o", "r", "l", "d", "!"]
-let example_test3: Split<'Hi! world!', '!'>; // ["Hi", " world"
-let example_test4: Split<'Hi! world!', '5645'>; // []
+let example_test1: Split<'Hi! world!', 'w'>;  // ["Hi! ", "orld!"]
+let example_test2: Split<'Hi! world!', ''>; //   ["H", "i", "!", " ", "w", "o", "r", "l", "d", "!"]
+let example_test3: Split<'Hi! world!', '!'>; // ["Hi", " world", ""]
+let example_test4: Split<'Hi! world!', '5645'>; // ['Hi! world!']
